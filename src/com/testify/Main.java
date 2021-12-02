8b5d0a95d86b828922afdc486901ecdcacd12cbe
lucy <azakalucy@gmail.com>
2021-12-02 00:02:55 +0000

    first java code

---
+++ src/com/testify/Main.java
@@ -0,0 +1,13 @@
+package com.testify;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("My name is Lucy.");
+        System.out.println("I am from Lagos Nigeria.");
+        System.out.println("I am an automated and manual QA engineer");
+        System.out.println("My goal is to get a befitting job as a QA engineer");
+
+
+    }
+}
